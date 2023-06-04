1fd1d7840c1929ea83dc0b93ea236314935028fc
fjpa654 <fjpa654@users.noreply.github.com>
2023-06-04 00:00:32 -0600

    add a github icon to rederect to my repositories on github

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,19 +3,25 @@ import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LinkedInIcon from '../assets/icons8-linkedin-50.png';
+import GitHubIcon from '../assets/icons8-github-64.png';
 
 const Contact = () => {
-  return (
-    <Container >
-      <Row>
-        <Col xs={12} className="text-center">
+    return (
+      <Container>
+        <Row className="justify-content-center align-items-center">
+          <Col xs={'auto'} className="mx-2">
             <a href="https://www.linkedin.com/in/francisco-padilla-248b5069/">
-                <Image src={process.env.PUBLIC_URL + LinkedInIcon} alt="LinkedIn Icon" />
+              <Image src={process.env.PUBLIC_URL + LinkedInIcon} alt="LinkedIn Icon" />
             </a>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
-
-export default Contact;
+          </Col>
+          <Col xs={'auto'} className="mx-2">
+            <a href="https://github.com/fjpa654?tab=repositories">
+              <Image src={process.env.PUBLIC_URL + GitHubIcon} alt="GitHub Icon" />
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+  
+  export default Contact;
