d7f68e5962bea02715de3ecc4b15d8682ef0ebb4
fjpa654 <fjpa654@users.noreply.github.com>
2023-06-04 13:00:02 -0600

    Update icons path

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,3 @@
-// src/components/Contact.js
 import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,17 +10,17 @@ const Contact = () => {
         <Row className="justify-content-center align-items-center">
           <Col xs={'auto'} className="mx-2">
             <a href="https://www.linkedin.com/in/francisco-padilla-248b5069/">
-              <Image src={process.env.PUBLIC_URL + LinkedInIcon} alt="LinkedIn Icon" />
+              <Image src={LinkedInIcon} alt="LinkedIn Icon" />
             </a>
           </Col>
           <Col xs={'auto'} className="mx-2">
             <a href="https://github.com/fjpa654?tab=repositories">
-              <Image src={process.env.PUBLIC_URL + GitHubIcon} alt="GitHub Icon" />
+              <Image src={GitHubIcon} alt="GitHub Icon" />
             </a>
           </Col>
         </Row>
       </Container>
     );
-  };
-  
-  export default Contact;
+};
+
+export default Contact;
